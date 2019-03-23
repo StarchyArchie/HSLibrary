cec920a0ab2a4cbc6bf11ad7113a18339ad4494c
vnigam <vishaldnigam@gmail.com>
2019-03-22 20:58:05 -0400

    COMPLETED

--- main/java/ser210/quinnipiac/edu/hearthapi/WelcomeActivity.java
+++ main/java/ser210/quinnipiac/edu/hearthapi/WelcomeActivity.java
@@ -1,6 +1,10 @@
 package ser210.quinnipiac.edu.hearthapi;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,13 +13,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     Button searchSet,searchSingle,searchClass,searchRarity;
     Intent intent;
     Toolbar toolbar;
-
+    int baseColor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,7 @@ public class WelcomeActivity extends AppCompatActivity {
         searchClass = findViewById(R.id.cardClass);
         searchRarity = findViewById(R.id.rarity);
         toolbar = findViewById(R.id.toolbar);
+        baseColor=Color.WHITE;
         final Intent intent = new Intent(this,CardRequestActivity.class);
         setSupportActionBar(toolbar);
 
@@ -60,20 +66,54 @@ public class WelcomeActivity extends AppCompatActivity {
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                return false;
+                switch(menuItem.getItemId()){
+                    case R.id.action_help:
+                        helpMenu(WelcomeActivity.this);
+                        break;
+
+                    case R.id.action_settings:
+                        RelativeLayout current = (RelativeLayout) findViewById(R.id.WelcomeLayout);
+
+                        if(baseColor==Color.WHITE){
+                            current.setBackgroundColor(Color.GREEN);
+                            baseColor=Color.GREEN;
+                        }
+                        else {
+                            current.setBackgroundColor(Color.WHITE);
+                            baseColor=Color.WHITE;
+                        }
+
+                        break;
+
+                    case R.id.action_share:
+
+                        break;
+
+                    default: return false;
+                }
+                return true;
             }
         });
+    }
+    public void helpMenu(Context context){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(context);
+        builder1.setMessage("This app offers four different search options for cards from the virtual card game Hearthstone. \n Choose to search directly by card name, the set it came in, it's rarity, or the character class it belongs to.\n This will grab the image of the card from the API and display it in the app.");
+        builder1.setCancelable(true);
+
+        builder1.setNegativeButton(
+                "Dismiss",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main,menu);
-        return true;
-    }
-    public boolean onOptionItemSelected(MenuItem item){
-        switch(item.getItemId()){
-            case R.id.action_settings:
-
-        }
         return true;
     }
 }
